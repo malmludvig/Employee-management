ebff3b72f69b97a448b70564f5d4684e3108d95d
Ludvig <malmludvig@gmail.com>
2020-11-22 20:45:13 +0100

    Created the skeleton for the project. 2 projects and one library

---
+++ UserUi/Program.cs
@@ -0,0 +1,58 @@
+﻿using EmployeeClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UserUi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string inputName = LoginAndValidation.UserLogin();
+
+            var EmployeeList = CSVOperations.GetEmployeesFromCSV();
+
+            while (true)
+            {
+
+                Console.WriteLine("Write 1 to print all Employees. \n" +
+                                  "Write 2 to view your info. \n" +
+                                  "Write 3 to edit your info. \n" +
+                                  "Write 4 to save Employees to CSV-file.\n");
+
+                string x = "";
+                x = Console.ReadLine();
+
+                if (x == "1")
+                {
+                    Console.Clear();
+                    CSVOperations.PrintEmployees(EmployeeList);
+                    continue;
+                }
+
+                if (x == "2")
+                {
+                    Console.Clear();
+                    CSVOperations.PrintCurrentUser(inputName, EmployeeList);
+                }
+
+                if (x == "3")
+                {
+                    CSVOperations.EditCurrentUser(inputName, EmployeeList);
+                    continue;
+                }
+
+
+                if (x == "4")
+                {
+                    CSVOperations.SaveToCSV(EmployeeList);
+                    continue;
+                }
+
+                x = "";
+            }
+        }
+    }
+}
---
+++ adminUi/Program.cs
@@ -0,0 +1,67 @@
+﻿using EmployeeClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UserUi
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var EmployeeList = Library.GetEmployeesFromCSV();
+            
+            LoginAndValidation.AdminLogin();
+            string inputName = "";
+
+            while (true)
+            {
+                
+                Console.WriteLine("Write 1 to print all Employees. \n" +
+                                  "Write 2 to create a new Employee. \n" +
+                                  "Write 3 to edit a Employee. \n" +
+                                  "Write 4 to save Employees to CSV-file.\n" +
+                                  "Write 5 to remove a Employee.");
+                
+                string x = "";
+                x = Console.ReadLine();
+
+                if (x == "1")
+                {
+                    Console.Clear();
+                    Library.PrintEmployees(EmployeeList);
+                    continue;
+                }
+
+                if (x == "2")
+                {
+                    Console.Clear();
+                    Library.AddEmployee(EmployeeList);
+                    continue;
+                }
+
+                if (x == "3")
+                {
+                    Console.Clear();
+                    Library.EditEmployee(inputName, EmployeeList);
+                    continue;
+                }
+
+                if (x == "4")
+                {
+                    Console.Clear();
+                    Library.SaveToCSV(EmployeeList);
+                    continue;
+                }
+
+                if (x == "5")
+                {
+                    Library.RemoveEmployee(EmployeeList);
+                    continue;
+                }
+            }
+        }
+    }
+}
