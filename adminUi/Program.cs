bb587d9a70c056359e4a02c2a10d7068356ee765
Ludvig <malmludvig@gmail.com>
2020-11-22 21:09:14 +0100

    Finished adminUi-Console application

--- adminUi/Program.cs
+++ adminUi/Program.cs
@@ -11,54 +11,54 @@ namespace UserUi
         static void Main(string[] args)
         {
 
-            var EmployeeList = Library.GetEmployeesFromCSV();
-            
+            var EmployeeList = CSVOperations.GetEmployeesFromCSV();
+
             LoginAndValidation.AdminLogin();
             string inputName = "";
 
             while (true)
             {
-                
+
                 Console.WriteLine("Write 1 to print all Employees. \n" +
                                   "Write 2 to create a new Employee. \n" +
                                   "Write 3 to edit a Employee. \n" +
                                   "Write 4 to save Employees to CSV-file.\n" +
                                   "Write 5 to remove a Employee.");
-                
+
                 string x = "";
                 x = Console.ReadLine();
 
                 if (x == "1")
                 {
                     Console.Clear();
-                    Library.PrintEmployees(EmployeeList);
+                    CSVOperations.PrintEmployees(EmployeeList);
                     continue;
                 }
 
                 if (x == "2")
                 {
                     Console.Clear();
-                    Library.AddEmployee(EmployeeList);
+                    CSVOperations.AddEmployee(EmployeeList);
                     continue;
                 }
 
                 if (x == "3")
                 {
                     Console.Clear();
-                    Library.EditEmployee(inputName, EmployeeList);
+                    CSVOperations.EditEmployee(inputName, EmployeeList);
                     continue;
                 }
 
                 if (x == "4")
                 {
                     Console.Clear();
-                    Library.SaveToCSV(EmployeeList);
+                    CSVOperations.SaveToCSV(EmployeeList);
                     continue;
                 }
 
                 if (x == "5")
                 {
-                    Library.RemoveEmployee(EmployeeList);
+                    CSVOperations.RemoveEmployee(EmployeeList);
                     continue;
                 }
             }
